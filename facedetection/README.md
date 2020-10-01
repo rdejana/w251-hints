896f225e2ca2d0a76392947a8c0ebba92915c0bb
Ryan <rdejana@us.ibm.com>
2020-10-01 11:52:01 -0600

    some updates

---
+++ facedetection/README.md
@@ -0,0 +1,11 @@
+# FaceDetection
+
+
+
+
+## Using BlazeFace
+BlazeFace is a fast, light-weight face detector from Google Research that builds on Single Shot MultiBox Detector (SSD).  This example uses the work done at https://github.com/hollance/BlazeFace-PyTorch to convert from a TFLite model to a PyTorch compatable one.  See https://github.com/hollance/BlazeFace-PyTorch for additional details.
+
+
+### Running
+
---
+++ facedetection/cam.py
@@ -0,0 +1,60 @@
+import numpy as np
+import torch
+import cv2
+from blazeface import BlazeFace
+
+print("PyTorch version:", torch.__version__)
+print("CUDA version:", torch.version.cuda)
+print("cuDNN version:", torch.backends.cudnn.version())
+
+gpu = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(gpu)
+
+net = BlazeFace().to(gpu)
+net.load_weights("blazeface.pth")
+net.load_anchors("anchors.npy")
+print("starting camera now....")
+
+
+cap = cv2.VideoCapture(1)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    dimensions  = frame.size
+    height = frame.shape[0]
+    width = frame.shape[1]
+    
+    print('Image Height       : ',height)
+    print('Image Width        : ',width)
+    # Our operations on the frame come here
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img,(128,128))
+    detections = net.predict_on_image(img)
+    if isinstance(detections, torch.Tensor):
+        detections = detections.cpu().numpy()
+
+    if detections.ndim == 1:
+        detections = np.expand_dims(detections, axis=0)
+    print("Found %d faces" % detections.shape[0])
+    if detections.shape[0] > 0 :
+        for i in range(detections.shape[0]):
+            ymin = detections[i, 0] * frame.shape[0]
+            xmin = detections[i, 1] * frame.shape[1]
+            ymax = detections[i, 2] * frame.shape[0]
+            xmax = detections[i, 3] * frame.shape[1]
+            print(xmin)
+            print(xmax)
+            print(ymin)
+            print(ymax)
+            cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 0, 0), 2)
+    #print(detections)
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
