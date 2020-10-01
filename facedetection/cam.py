930d2e3e798bcf9c08aa780a9decafa0dd6b40eb
Ryan DeJana <rdejana@us.ibm.com>
2020-10-01 11:57:42 -0600

    update cam.py

--- facedetection/cam.py
+++ facedetection/cam.py
@@ -3,6 +3,7 @@ import torch
 import cv2
 from blazeface import BlazeFace
 
+# some useful info
 print("PyTorch version:", torch.__version__)
 print("CUDA version:", torch.version.cuda)
 print("cuDNN version:", torch.backends.cudnn.version())
@@ -13,20 +14,16 @@ print(gpu)
 net = BlazeFace().to(gpu)
 net.load_weights("blazeface.pth")
 net.load_anchors("anchors.npy")
+# let's start the capture now
 print("starting camera now....")
 
-
-cap = cv2.VideoCapture(1)
+#adjust based on your device.  For most cases, normally 0
+cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    dimensions  = frame.size
-    height = frame.shape[0]
-    width = frame.shape[1]
     
-    print('Image Height       : ',height)
-    print('Image Width        : ',width)
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -40,16 +37,12 @@ while(True):
     print("Found %d faces" % detections.shape[0])
     if detections.shape[0] > 0 :
         for i in range(detections.shape[0]):
+            # adjust the coordinates to the original image
             ymin = detections[i, 0] * frame.shape[0]
             xmin = detections[i, 1] * frame.shape[1]
             ymax = detections[i, 2] * frame.shape[0]
             xmax = detections[i, 3] * frame.shape[1]
-            print(xmin)
-            print(xmax)
-            print(ymin)
-            print(ymax)
             cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 0, 0), 2)
-    #print(detections)
     # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
